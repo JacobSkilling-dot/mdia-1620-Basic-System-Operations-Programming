1e66518c0b20314adb8b83042440646467d0f4c7
Jacob Skilling <jskilling1@my.bcit.ca>
2022-09-16 12:15:27 -0700

    adding more code

---
+++ Part-1/Week-1/test.js
@@ -0,0 +1,42 @@
+// camelCase
+var bugsCollected = 4;
+
+// pascalCase
+var BugsCollected = 4;
+
+// Snake_Case
+var Bugs_Collected = 4;
+
+// Use camelCase
+
+var electronicBox = "cpu";
+
+// Boolean
+// Initialize the var called proceed to the value true.
+
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++; // i = 1;
+    proceed = false;
+};
+
+console.log(i);
+
+// Data Type - Object
+/**
+ * 6-4-1 create a variable called grannySmith and add the
+ * following values (use camelcase):
+ * colour of the apple is green 
+ * origin location is australia
+ * origin date is 1868
+ */
+
+var grannySmith = {
+   colour: "green",
+   originLocation: "Australia",
+   originDate: "1868"
+};
+
+console.log(grannySmith);
